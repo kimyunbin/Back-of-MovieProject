a2f2a3e50bd8417bdf5d44d651cc84fcab415fbd
swhan9404 <swhan9404@naver.com>
2021-05-24 13:06:50 +0900

    tournament 완성

--- final_back/settings.py
+++ final_back/settings.py
@@ -30,14 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+
     'accounts',
     'movies',
     'community',
-
-    'corsheaders',
-    'rest_framework',
-    'drf_yasg',
-
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
